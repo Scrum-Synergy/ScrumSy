b4351b0539a69a6e88fb0d82fc6429349de42524
Koussay Hajri <hajrikoussay03@gmail.com>
2024-03-03 20:41:08 +0100

    mongoDB Connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 // Server Side -> Mohamed , Koussay 
-require("dotenv").config(); // access env file
-const express = require("express");
+import dotenv from "dotenv"; // access env file
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config(); // Access env file
+
 const app = express();
-const cors = require("cors");
 
 // Origin Client Side
 app.use(express.json());
 app.use(cors());
 
-// Starting Server PORT localhost:3001
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => { console.log(`We Connecting PORT -> ${PORT}`); });
+const MONGODB_URL = process.env.MONGODB_URL;
+
+// Connecting to MongoDB
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.log('Error connecting to MongoDB:', err.message);
+    });
+
+
