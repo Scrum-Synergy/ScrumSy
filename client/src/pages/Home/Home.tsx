7ea88b419fce762b2b8a9a7ca0f7827496a20190
Koussay Hajri <hajrikoussay03@gmail.com>
2024-03-14 14:54:59 +0100

    update Home.tsx

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,11 +3,19 @@ import NavBar from "../../components/NavBar";
 const Home = () => {
   return (
     <>
-    
-      <div className="background h-[100vh] w-[100%] bg-cover bg-no-repeat ">
+      <div
+        className="background h-[100vh] w-[100%] bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: "url('hero.jpg')",
+          position: "absolute",
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center"
+        }}
+      >
         <NavBar />
       </div>
-      
     </>
   );
 };
